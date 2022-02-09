57d7950d14d38b4c456cda6c2decdc5fd625bd60
Patorn Utenpattanun <patorn.u@gmail.com>
2022-02-09 20:46:50 +0700

    add vvix data

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import yfinance as yf
 
-data = yf.download("^VIX", interval="1m", period="7d")
+interval = '1m'
+period = '7d'
+
+data = yf.download("^VIX", interval=interval, period=period)
 data.to_csv('data/vix_1m_7d.csv')
+
+data = yf.download("^VVIX", interval=interval, period=period)
+data.to_csv('data/vvix_1m_7d.csv')
