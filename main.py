68f1cc77e822410bdec8914a96cfb3731c882874
Patorn Utenpattanun <patorn.u@gmail.com>
2022-02-09 20:55:01 +0700

    Add retry

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import yfinance as yf
+import time
 
 interval = '1m'
-period = '7d'
+period = '2d'
+max_attempt = 3
 
-data = yf.download("^VIX", interval=interval, period=period)
-data.to_csv('data/vix_1m_7d.csv')
+def download(ticker, fname, interval, period, max_attempt):
+    for attempt in range(max_attempt):
+        time.sleep(2)
+        try:
+            data = yf.download(ticker, interval=interval, period=period)
+            data.to_csv(fname)
+        except Exception as e:
+            if attempt < max_attempt - 1:
+                print('Attempt {}: {}'.format(attempt + 1, str(e)))
+            else:
+                raise
+        else:
+            break
 
-data = yf.download("^VVIX", interval=interval, period=period)
-data.to_csv('data/vvix_1m_7d.csv')
+if __name__ == "__main__":
+    print("Downloading VIX")
+    download('^VIX', 'data/vix_1m.csv', interval, period, max_attempt)
+    print("Downloading VVIX")
+    download('^VVIX', 'data/vvix_1m.csv', interval, period, max_attempt)
